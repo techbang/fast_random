1410717c282734a2965b32ef105dffdc852be3c4
Chun-wei Kuo <Dendoh@gmail.com>
2012-08-07 14:48:35 +0800

    Fix deprecation warning in Rails 3.2

--- lib/fast_random.rb
+++ lib/fast_random.rb
@@ -8,8 +8,4 @@ module FastRandom
       joins("join (SELECT CEIL(RAND() * (SELECT MAX(id) FROM #{table_name})) AS gid) AS r2").where("#{table_name}.id > r2.gid")
     end
   end
-  
-  module InstanceMethods
-  end
-  
 end
